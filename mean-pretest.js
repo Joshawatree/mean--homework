68dd06dee845b377dce107dd86ad76656d1adc59
Joshawa Roberts <joshawa.roberts@gmail.com>
2015-04-14 16:38:32 -0600

    added pretest

--- mean-pretest.js
+++ mean-pretest.js
@@ -1,3 +1,4 @@
+// 1.)
 var myObject = {
 	firstName: "Joshawa",
 	lastName: "Roberts",
@@ -7,11 +8,46 @@ var myObject = {
 };
 console.log(myObject.fullName());
 
+// 2.)
+var countStart = 1;
+var countEnd = 15;
 
+var arr = [];
 
+while(countStart < countEnd+1){
+  if (countStart % 2 === 0){
+    arr.push(countStart++ + "*");
+  } else {
+    arr.push(countStart++);
+  }
+}
+console.log(arr);
 
+// 3.) Not quite there yet. Still need to figure out how to choose all vars defined in the object.
+function person(choose) {
+  var name = "Joshawa Roberts";
+  var age = 23;
+  var height = 74; //inches;
+  var hometown = "Naperville, IL";
+  
+  var choice = '';
+  switch(choose){
+    case 'name':
+      choice = name;
+      break;
+    case 'age':
+      choice = age;
+      break;
+    case 'height':
+      choice = height;
+      break;
+    case 'hometown':
+      choice = hometown;
+      break;
+  }
+  
+  return choice;
+}
 
-name: Joshawa
-age: 23
-height: 6'2"
-hometown: Naperville, Illinois
+console.log(person('age'));
+
