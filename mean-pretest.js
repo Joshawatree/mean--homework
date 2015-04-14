3467f0c0ac8e8d6c5298bd2ab10c1abfc9d5ace7
Joshawa Roberts <joshawa.roberts@gmail.com>
2015-04-13 18:23:58 -0600

    initial commit

---
+++ mean-pretest.js
@@ -0,0 +1,17 @@
+var myObject = {
+	firstName: "Joshawa",
+	lastName: "Roberts",
+	fullName: function() { 'use strict';
+		return this.lastName + ', ' + this.firstName;
+	}
+};
+console.log(myObject.fullName());
+
+
+
+
+
+name: Joshawa
+age: 23
+height: 6'2"
+hometown: Naperville, Illinois
